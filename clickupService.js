b9305ee977c388f9c99e8ed215f1f739bcfecbe0
jaredparis <jaredparis85@gmail.com>
2026-03-31 11:27:18 -0400

    updated buildClikcupPayload removed additonal custuom fields

--- clickupService.js
+++ clickupService.js
@@ -33,43 +33,18 @@ async function buildClickUpPayload(canonical) {
 
     description: [
       `Created by HubSpot sync`,
+      `HubSpot Deal ID: ${canonical.hubspot_deal_id || ''}`,
+      `HubSpot Company ID: ${canonical.hubspot_company_id || ''}`,
+      `Store name: ${canonical.dba_store_name || ''}`,
       `Market: ${canonical.city && canonical.state ? `${canonical.city}, ${canonical.state}` : ''}`,
       `Number of locations: ${canonical.location_count || ''}`,
-      `Store name: ${canonical.dba_store_name || ''}`,
-      `Hubspot Company ID: ${canonical.hubspot_company_id || ''}`,
-      `Hubspot Deal ID: ${canonical.hubspot_deal_id || ''}`,
     ].join('\n'),
 
     custom_fields: [
       {
         id: '9c30c95b-e8b9-4a8d-86b5-57444a7dc061',
         value: canonical.hubspot_deal_id,
-      },
-      {
-        id: 'e2ffb995-05b5-403b-9fb5-64773323242d',
-        value: canonical.hubspot_company_id,
-      },
-      {
-        id: '107dcb28-f0df-4b53-a6f3-d11950d5c503',
-        value: canonical.legal_entity_name || '',
-      },
-      {
-        id: 'fa57bbd3-235a-4334-9ff9-b8025458fb4e',
-        value: canonical.dba_store_name || '',
-      },
-      {
-        id: '98fc7f0b-f93e-438b-99a6-b4a047ff236f',
-        value: canonical.location_count
-          ? Number(canonical.location_count)
-          : null,
-      },
-      {
-        id: '05c402cd-5ba4-4db2-8b78-4ed70959a9de',
-        value:
-          canonical.city && canonical.state
-            ? `${canonical.city}, ${canonical.state}`
-            : '',
-      },
+      }
     ],
   };
 }
